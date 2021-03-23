64bb506a8baf39f30866e340ec44db23c4e61901
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-23 15:52:04 +0100

    Creo file base e collego Vue

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Descrizione
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+//Instanzio Vue
+var app = new Vue(
+  {
+    //collego vue al DOM
+    el: '#root',
+  }
+)
+;
+
+console.log('ok');
